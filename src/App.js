74aa0c1824b54e033e4a00aff31585eabee14c6d
cbr-Mikhail <mgp-nsk@yandex.ru>
2024-02-07 10:17:37 +0700

    project builded by babel&webpack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-export default function App() {
+import React from "react";
+
+ const App = function() {
     return (
         <div class="alert alert-success">
             React root-component rendered in div#root
             </div>
     )
 }
+
+export default App;
